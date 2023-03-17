368d0c63457f2bd487bd26e336269416898b6393
IgorMichels <igorxadrezbr@gmail.com>
2023-03-17 10:21:50 -0300

    removing unused files

--- Project_1/begin.py
+++
@@ -1,7 +0,0 @@
-n = 300
-with open('shakespeare.txt', 'r') as f: text = f.readlines()
-text = ''.join(text)
-
-with open('shakespeare_extended.txt', 'w') as f:
-    for i in range(n):
-        f.write(text)
--- Project_1/end.py
+++
@@ -1,3 +0,0 @@
-import os
-os.remove('shakespeare_extended.txt')
-os.remove('shakespeare')
