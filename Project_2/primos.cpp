728ba1408538e8a55c80ae8ba7bb7b2b4e6ba693
IgorMichels <igorxadrezbr@gmail.com>
2023-03-27 19:09:17 -0300

    new prime function

--- Project_2/primos.cpp
+++ Project_2/primos.cpp
@@ -3,13 +3,15 @@
 #include <string>
 #include <vector>
 #include <numeric>
-#include <bits/stdc++.h>
+// #include <bits/stdc++.h>
 #include <chrono>
 #include <cmath>
 #include <thread>
 
-auto start0 = std::chrono::steady_clock::now();
-std::vector<int> primos {};
+using namespace std;
+
+auto start0 = chrono::steady_clock::now();
+vector<int> primos {};
 
 
 //// function below doesnt work with threads
@@ -27,6 +29,26 @@ std::vector<int> primos {};
 //     return true;
 // }
 
+
+bool is_prime2(int p, int MAX_TEST = 100) {
+    if (p < 2) return false;
+    
+    if (p < 4) {
+        primos.push_back(p);    
+        return true;
+    }
+
+    // filtering even numbers and multiples of 3
+    if ((p % 6 != 1) && (p % 6 != 5)) return false;
+    double rp = pow(p, 0.5);
+    for (int factor = 5; factor <= rp; factor += 2) {
+        if (p % factor == 0) return false;
+    }
+
+    primos.push_back(p);
+    return true;
+}
+
 bool is_prime(int p){
 
     if(p < 2){return false;}
@@ -34,7 +56,7 @@ bool is_prime(int p){
         primos.push_back(p);
         return true;}
 
-    double rp = std::pow(p, 0.5);
+    double rp = pow(p, 0.5);
     for(int i = 2; i <= rp; i++){
         if(p % i == 0){return false;}
     }
@@ -43,8 +65,9 @@ bool is_prime(int p){
     return true;
 }
 
-void write(std::vector<int> * primes, std::string file_name = "primos.txt"){
-    std::ofstream myfile;
+
+void write(vector<int> * primes, string file_name = "primos.txt"){
+    ofstream myfile;
     myfile.open (file_name);
     for(int i = 0; i < (*primes).size(); i++){
         myfile << (*primes)[i];
@@ -64,67 +87,67 @@ void iterate(int begin, int end){
 void n_balanced(int n_threads, int N){
 
     int block = (n_threads - 1) / N;
-    std::vector<std::thread *>  threads;
+    vector<thread *>  threads;
 
     int k = 1;
     for(int i = 0; i < n_threads - 1; i++){
-        std::thread * ti = new std::thread(&iterate, k, k + block);
+        thread * ti = new thread(&iterate, k, k + block);
         threads.push_back(ti);
         k += block;
     }
-    std::thread *ti = new std::thread(&iterate, k, N);
+    thread *ti = new thread(&iterate, k, N);
     threads.push_back(ti);
 
-    for (std::thread * th : threads) {
+    for (thread * th : threads) {
         th -> join();
     }
 
     return;
 }
 
-void printP(int c, std::vector<int> * vec, int k = 20){
+void printP(int c, vector<int> * vec, int k = 20){
 
     for(int i = 0; i < k; i++){
         if(c >= (*vec).size()){
-            std::cout << "\nThe list is over! \n";
+            cout << "\nThe list is over! \n";
             return;
         }
         else if(c % 10 == 0){
-            std::cout << "\n[" << c << "-" << c+10 << "]: "  << (*vec)[c];
+            cout << "\n[" << c << "-" << c+10 << "]: "  << (*vec)[c];
         }
         else{
-            std::cout << " | " << (*vec)[c];
+            cout << " | " << (*vec)[c];
         }
         c++;
     }
     int _k;
-    std::cout << "\n \nHow many more primes to display? (type 0 to exit): ";
-    std::cin >> _k;
+    cout << "\n \nHow many more primes to display? (type 0 to exit): ";
+    cin >> _k;
     if(_k == 0){
-        std::cout << "exit! \n \n";
+        cout << "exit! \n \n";
         return;
     }
     else{
-        std::cout << "\nDisplaying next " << _k << " primes: \n";
+        cout << "\nDisplaying next " << _k << " primes: \n";
         printP(c, &(*vec), _k);
     }
 
     return;
 }
 
-void print(std::chrono::duration<double> prepTime, std::chrono::duration<double> totalTime,
-           int N, std::vector<int> * vec, int numThreads = 1, bool PRINT = true){
+void print(chrono::duration<double> prepTime, chrono::duration<double> totalTime,
+           int N, vector<int> * vec, int numThreads = 1, bool PRINT = true){
 
     int Np = (*vec).size();
-    std::cout << "---\n";
-    std::cout << "Results:\n";
-    std::cout << "    - numThreads: " << numThreads << '\n';
-    std::cout << "    - search time: " << totalTime.count() << "s" << '\n';
-    std::cout << "    - amount evaluated: " << N << '\n';
-    std::cout << "    - number of primes: " << Np << '\n';
+    cout << "---\n";
+    cout << "Results:\n";
+    cout << "    - numThreads: " << numThreads << '\n';
+    cout << "    - search time: " << totalTime.count() << "s" << '\n';
+    cout << "    - amount evaluated: " << N << '\n';
+    cout << "    - number of primes: " << Np << '\n';
 
     if(PRINT){
-        std::cout << "\nDisplaying first 20 primes: \n";
+        cout << "\nDisplaying first 20 primes: \n";
         printP(0, &(*vec), 20);
     }
     return;
@@ -133,24 +156,26 @@ void print(std::chrono::duration<double> prepTime, std::chrono::duration<double>
 
 int main () {
 
-    int n_ = std::pow(10, 4);
+    int n_ = pow(10, 6);
 
-    auto end0 = std::chrono::steady_clock::now();
-    std::chrono::duration<double> diff0 = end0 - start0;
-    std::cout << "---\n";
-    std::cout << "preparation time: " << diff0.count() << '\n';
+    auto end0 = chrono::steady_clock::now();
+    chrono::duration<double> diff0 = end0 - start0;
+    cout << "---\n";
+    cout << "preparation time: " << diff0.count() << '\n';
 
-    std::vector<int> vec_n_threads = {1,2,4,8,16,30,50,100};
-    for(int n_threads : vec_n_threads){
+    vector<int> vec_n_threads = {1,2,4,8,16,30,50,100};
+    int MAX_THREADS = 10;
+    // for(int n_threads : vec_n_threads){
+    for(int n_threads = 1; n_threads <= MAX_THREADS; n_threads++){
 
         primos = {};
-        auto start = std::chrono::steady_clock::now();
+        auto start = chrono::steady_clock::now();
         n_balanced(n_threads, n_);
-        std::sort(primos.begin(), primos.end());
+        sort(primos.begin(), primos.end());
         write(&primos);
 
-        auto end = std::chrono::steady_clock::now();
-        std::chrono::duration<double> diff = end - start;
+        auto end = chrono::steady_clock::now();
+        chrono::duration<double> diff = end - start;
 
         if(n_threads == 100){
             print(diff0, diff, n_, &primos, n_threads, true);
