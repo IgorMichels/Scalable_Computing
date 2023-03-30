612b989e018f1264f57ca343996f598e02b8787d
IgorMichels <igorxadrezbr@gmail.com>
2023-03-30 15:53:10 -0300

    added mock structure

---
+++ Project_A1/mock.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+class Car:
+    def __init__(self,
+                 maxSpeed : int,
+                 maxAcceleration : float,
+                 maxDesacceleration : float):
+        
+        pass
+
+class Highway:
+    def __init__(self,
+                 code : int,
+                 numLanesN : int,
+                 numLanesS : int,
+                 probNewCar : float,
+                 probCrash : float,
+                 probChangeLane : float):
+        
+        pass
+
+    def simulate(self,
+                 epochs : int):
+        
+        pass
+
+if __name__ == '__main__':
+    pass
