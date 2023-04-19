921861506ea1e9a2fa6df44d53e0bba9b4c04420
IgorMichels <igorxadrezbr@gmail.com>
2023-04-19 00:42:43 -0300

    changing data structures

--- Project_A1/readFiles.cpp
+++ Project_A1/readFiles.cpp
@@ -1,3 +1,4 @@
+#include <sys/time.h>
 #include <iostream>
 #include <dirent.h>
 #include <fstream>
@@ -7,9 +8,38 @@
 #include <string>
 #include <chrono>
 #include <tuple>
+#include <ctime>
+#include <mutex>
 #include <map>
 
+#include "API_query.h"
+
 using namespace std;
+
+struct highwayData {
+    int maxSpeed = 0;
+    int carMaxSpeed = 0;
+    string infoTime = "";
+    mutex carDataBlocker;
+    mutex highwayDataBlocker;
+};
+
+struct carData {
+    int lane = 0;
+    int actualPosition = 0;
+    int lastPosition = 0;
+    int penultimatePosition = 0; // sim, essa palavra existe, só não é muito usada (https://www.italki.com/en/post/question-92435)
+    bool isInHighway = true;
+    string name = "";
+    string model = "";
+    int year = 1900;
+    int speed = 0;
+    int acceleration = 0;
+    bool canCrash = false;
+};
+
+map<int, map<string, carData>*> carInfos;
+map<int, highwayData*> highwayInfos;
 
 vector<string> getFiles() {
     DIR *dr;
@@ -45,21 +75,27 @@ tuple<string, int, int> parseRow(string row)
     return make_tuple(plate, actualLane, lanePosition);
 }
 
-void updateData(map<string, int[4]> *dict, string row) {
+void updateData(map<string, carData> *carInfos, highwayData *hwData, string row) {
     string plate;
     int actualLane;
     int lanePosition;
     tie(plate, actualLane, lanePosition) = parseRow(row);
-
-    // IMPORTANTE: barrar leitura aqui
-    (*dict)[plate][0] = actualLane;
-    (*dict)[plate][3] = (*dict)[plate][2];
-    (*dict)[plate][2] = (*dict)[plate][1];
-    (*dict)[plate][1] = lanePosition;
-    // liberar leitura
+    if (plate.length() > 7) {
+        (*hwData).highwayDataBlocker.lock(); // barrar leitura aqui
+        (*hwData).infoTime = plate;
+        (*hwData).highwayDataBlocker.unlock(); // liberar leitura
+    }
+    else {
+        (*hwData).carDataBlocker.lock(); // barrar leitura aqui
+        (*carInfos)[plate].lane = actualLane;
+        (*carInfos)[plate].penultimatePosition = (*carInfos)[plate].lastPosition;
+        (*carInfos)[plate].lastPosition = (*carInfos)[plate].actualPosition;
+        (*carInfos)[plate].actualPosition = lanePosition;
+        (*hwData).carDataBlocker.unlock(); // liberar leitura
+    }
 }
 
-void readFiles(string fileName, map<int, map<string, int[4]>*> *carPositions) { //, map<int, int[2]> highwayDetails) {
+void readFiles(string fileName, map<int, map<string, carData>*> *carInfos, map<int, highwayData*> *highwayInfos) {
     fstream file;
     file.open(fileName, ios::in);
     if (file.is_open()) {
@@ -75,38 +111,58 @@ void readFiles(string fileName, map<int, map<string, int[4]>*> *carPositions) { 
             lineController++;
             if (lineController == 1) {
                 highway = getInfo(row);
+                if ((*carInfos).find(highway) == (*carInfos).end()) (*carInfos)[highway] = new map<string, carData>;
+                if ((*highwayInfos).find(highway) == (*highwayInfos).end()) (*highwayInfos)[highway] = new highwayData;
             }
             else if (lineController == 2) {
-                speedLimitHighway = getInfo(row);
+                (*(*highwayInfos)[highway]).maxSpeed = getInfo(row);
             }
             else if (lineController == 3) {
-                speedLimitCar = getInfo(row);
+                (*(*highwayInfos)[highway]).carMaxSpeed = getInfo(row);
             }
             else {
-                if ((*carPositions).find(highway) == (*carPositions).end()) (*carPositions)[highway] = new map<string, int[4]>;
-                updateData((*carPositions)[highway], row);
+                updateData((*carInfos)[highway], &(*(*highwayInfos)[highway]), row);
             }
         }
         file.close();
     }
-    remove(fileName.c_str());
+    // remove(fileName.c_str());
 }
 
 int main() {
-    // dicionário externo: highway para outro dicionário
-    // dicionário interno: plate para [lane, pos, pos - 1, pos -2]
-    map<int, map<string, int[4]>*> infos;
-
     auto start = chrono::steady_clock::now();
     vector<string> files = getFiles();
-    for (auto file : files) readFiles(file, &infos);
+    for (auto file : files) readFiles(file, &carInfos, &highwayInfos);
     auto end = chrono::steady_clock::now();
 
-    // cout << (*(infos[101]))["LKT1F56"][0] << endl;
-    // cout << (*(infos[101]))["LKT1F56"][1] << endl;
-    // cout << (*(infos[101]))["LKT1F56"][2] << endl;
-    // cout << (*(infos[101]))["LKT1F56"][3] << endl;
+    cout << "DBL9H83" << endl;
+    cout << (*(carInfos[101]))["DBL9H83"].lane << endl;
+    cout << (*(carInfos[101]))["DBL9H83"].actualPosition << endl;
+    cout << (*(carInfos[101]))["DBL9H83"].lastPosition << endl;
+    cout << (*(carInfos[101]))["DBL9H83"].penultimatePosition << endl;
 
+    cout << endl;
+    cout << (*(highwayInfos[101])).infoTime << endl;
+    cout << (*(highwayInfos[101])).maxSpeed << endl;
+    cout << (*(highwayInfos[101])).carMaxSpeed << endl;
+
+    // cout << "\nTestando mock de dados externos" << endl;
+    // MyClass API;
+    // string plate = "DBL9H83";
+    // API.search(plate);
+    // cout << API.get_name() << endl;
+    // cout << API.get_model() << endl;
+    // cout << API.get_year() << endl;
+
+    // (*(carInfos[101]))[plate].name = API.get_name();
+    // (*(carInfos[101]))[plate].model = API.get_model();
+    // (*(carInfos[101]))[plate].year = API.get_year();
+
+    // cout << (*(carInfos[101]))[plate].name << ' ' << API.get_name() << endl;
+    // cout << (*(carInfos[101]))[plate].model << ' ' << API.get_model() << "batata" << endl;
+    // cout << (*(carInfos[101]))[plate].year << ' ' << API.get_year() << endl;
+
+    cout << endl;
     chrono::duration<double> totalTime = end - start;
     cout << totalTime.count() << endl;
     return 0;
