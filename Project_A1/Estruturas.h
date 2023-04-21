4cbbea590674671d6d1edfb675afa4e8a19b014d
Marcos A A <marcosantonioalves358@gmail.com>
2023-04-21 16:47:58 -0300

    arquivos de testes

---
+++ Project_A1/Estruturas.h
@@ -0,0 +1,289 @@
+#include <sys/time.h>
+#include <algorithm>
+#include <iostream>
+#include <dirent.h>
+#include <fstream>
+#include <sstream>
+#include <thread>
+#include <vector>
+#include <string>
+#include <chrono>
+#include <tuple>
+#include <ctime>
+#include <mutex>
+#include <map>
+#include <cstring>
+
+#include "API.h"
+
+using namespace std;
+
+
+struct highwayData {
+    int maxSpeed = 0;
+    int carMaxSpeed = 0;
+    string infoTime = "";
+    mutex carDataBlocker;
+    mutex highwayDataBlocker;
+};
+
+struct carData {
+    int lane = 0;
+    int actualPosition = 0;
+    int lastPosition = 0;
+    int penultimatePosition = 0; // sim, essa palavra existe, só não é muito usada (https://www.italki.com/en/post/question-92435)
+    bool isInHighway = true;
+    string name = "";
+    string model = "";
+    int year = 1900;
+    int speed = 0;
+    int acceleration = 0;
+    bool canCrash = false;
+};
+
+
+vector<string> getFiles() {
+    DIR *dr;
+    struct dirent *en;
+    vector<string> files;
+    const string folder = "files/";
+    dr = opendir("files/");
+    if (dr) {
+        while ((en = readdir(dr)) != NULL) {
+            if (strcmp(en->d_name, ".") == 0 || strcmp(en->d_name, "..") == 0) continue;
+            files.push_back(folder + en->d_name);
+        }
+        closedir(dr);
+    }
+    sort(files.begin(), files.end());
+    return files;
+}
+
+int getInfo(string row) {
+    return stoi(row.substr(row.find(' ') + 1));
+}
+
+tuple<string, int, int> parseRow(string row)
+{
+    int pos = row.find(',');
+    string plate = row.substr(0, pos);
+    string position = row.substr(pos + 2);
+    pos = position.find(',');
+    int actualLane = stoi(position.substr(1, pos - 1));
+    int lanePosition = stoi(position.substr(pos + 2, position.find(')') - pos - 2));
+    
+    return make_tuple(plate, actualLane, lanePosition);
+}
+
+vector<string> getBlocks(string text, int numBlocks) {
+    vector<string> dataBlocks;
+    size_t pos;
+    string aux;
+    string rows;
+    int blockSize = text.length() / numBlocks;
+    while (text != "") {
+        if (blockSize > text.length()) {
+            dataBlocks.push_back(text);
+            break;
+        }
+        aux = text.substr(blockSize);
+        pos = blockSize + aux.find('\n');
+        rows = text.substr(0, pos);
+        text.erase(0, pos + 1);
+        dataBlocks.push_back(rows);
+    }
+
+    return dataBlocks;
+}
+
+void updateDataBK(map<string, carData> *carInfos, highwayData *highwayInfos, string text) {
+    int pos;
+    string row;
+    string plate;
+    int actualLane;
+    int lanePosition;
+    cout << text << endl << "deu" << endl << endl << endl;
+    (*highwayInfos).carDataBlocker.lock(); // barrar leitura aqui
+    while ((pos = text.find('\n')) != string::npos) {
+        row = text.substr(0, pos);
+        cout << row << endl;
+        text.erase(0, pos + 1);
+        tie(plate, actualLane, lanePosition) = parseRow(row);
+        // (*highwayInfos).carDataBlocker.lock(); // barrar leitura aqui
+        (*carInfos)[plate].lane = actualLane;
+        (*carInfos)[plate].penultimatePosition = (*carInfos)[plate].lastPosition;
+        (*carInfos)[plate].lastPosition = (*carInfos)[plate].actualPosition;
+        (*carInfos)[plate].actualPosition = lanePosition;
+        (*carInfos)[plate].isInHighway = true;
+        // (*highwayInfos).carDataBlocker.unlock();
+    }
+
+    // cout << endl << text << endl << text.find('\n') << endl;
+    tie(plate, actualLane, lanePosition) = parseRow(text);
+    if (plate.length() == 7) {
+        // (*highwayInfos).carDataBlocker.lock(); // barrar leitura aqui
+        (*carInfos)[plate].lane = actualLane;
+        (*carInfos)[plate].penultimatePosition = (*carInfos)[plate].lastPosition;
+        (*carInfos)[plate].lastPosition = (*carInfos)[plate].actualPosition;
+        (*carInfos)[plate].actualPosition = lanePosition;
+        (*carInfos)[plate].isInHighway = true;
+        // (*highwayInfos).carDataBlocker.unlock(); // liberar leitura
+    }
+    else {
+        (*highwayInfos).highwayDataBlocker.lock(); // barrar leitura aqui
+        (*highwayInfos).infoTime = plate;
+        (*highwayInfos).highwayDataBlocker.unlock(); // liberar leitura
+    }
+    (*highwayInfos).carDataBlocker.unlock();
+}
+
+void updateData(map<string, carData> *carInfos, highwayData *highwayInfos, string text) {
+    int pos;
+    string row;
+    string plate;
+    int actualLane;
+    int lanePosition;
+    while (text.length() > 0) {
+        pos = text.find('\n');
+        if (pos == -1) pos = text.length();
+        row = text.substr(0, pos);
+        text.erase(0, pos + 1);
+        tie(plate, actualLane, lanePosition) = parseRow(row);
+            if (plate.length() == 7) {
+            (*highwayInfos).carDataBlocker.lock(); // barrar leitura aqui
+            (*carInfos)[plate].lane = actualLane;
+            (*carInfos)[plate].penultimatePosition = (*carInfos)[plate].lastPosition;
+            (*carInfos)[plate].lastPosition = (*carInfos)[plate].actualPosition;
+            (*carInfos)[plate].actualPosition = lanePosition;
+            (*carInfos)[plate].isInHighway = true;
+            (*highwayInfos).carDataBlocker.unlock(); // liberar leitura
+        }
+        else {
+            (*highwayInfos).highwayDataBlocker.lock(); // barrar leitura aqui
+            (*highwayInfos).infoTime = plate;
+            (*highwayInfos).highwayDataBlocker.unlock(); // liberar leitura
+        }
+    }
+}
+
+void readFiles(string fileName, int maxBlocks, map<int, map<string, carData>*> *carInfos, map<int, highwayData*> *highwayInfos) {
+    string row;
+    size_t pos;
+    string text;
+    int highway;
+    fstream file;
+    string plate;
+    int actualLane;
+    int lanePosition;
+    int speedLimitCar;
+    int speedLimitHighway;
+    vector<thread *> threads;
+    vector<string> dataBlocks; 
+
+    file.open(fileName, ios::in);
+    if (file.is_open()) {
+        file.seekg(0, file.end);
+        long int fileLength = file.tellg();
+        file.seekg(0, file.beg);
+
+        vector<char> buffer(fileLength);
+        if (!file.read(buffer.data(), fileLength)) {
+            perror("[ERROR] Failed to read the input file\n");
+            exit(EXIT_FAILURE);
+        }
+
+        file.close();
+
+        text = string(&buffer[0], fileLength);
+        for (int i = 0; i < 3; i++) {
+            pos = text.find('\n');
+            row = text.substr(0, pos);
+            text.erase(0, pos + 1);
+            if (i == 0) {
+                highway = getInfo(row);
+                
+                if ((*carInfos).find(highway) == (*carInfos).end()) (*carInfos)[highway] = new map<string, carData>;
+                if ((*highwayInfos).find(highway) == (*highwayInfos).end()) (*highwayInfos)[highway] = new highwayData;
+            }
+            else if (i == 1) (*(*highwayInfos)[highway]).maxSpeed = getInfo(row);
+            else (*(*highwayInfos)[highway]).carMaxSpeed = getInfo(row);
+        }
+        
+        if ((*(*highwayInfos)[highway]).infoTime != "") {
+            for (auto item : (*(*carInfos)[highway])) (*(*carInfos)[highway])[item.first].isInHighway = false;
+        }
+
+        int numBlocks;
+        if (text.length() / 200 + 1 < maxBlocks) numBlocks = text.length() / 200 + 1;
+        else numBlocks = maxBlocks;
+
+        vector<string> dataBlocks = getBlocks(text, numBlocks);
+        numBlocks = dataBlocks.size();
+        for (int i = 0; i < numBlocks; i++) {
+            thread *ti = new thread(&updateData, (*carInfos)[highway], &(*(*highwayInfos)[highway]), dataBlocks[i]);
+            threads.push_back(ti);
+        }
+        
+        for (auto th : threads) th -> join();
+        
+        vector<string> remove;
+        if ((*(*highwayInfos)[highway]).infoTime != "") {
+            for (auto item : (*(*carInfos)[highway])) {
+                // cout << item.first << " está sendo testado para apagar: " << item.second.isInHighway << endl;
+                if (item.second.isInHighway == false) remove.push_back(item.first);
+            }
+        }
+        for (auto plate : remove) (*(*carInfos)[highway]).erase(plate);
+    }
+    // remove(fileName.c_str());
+}
+
+
+void checkCollisions(map<int, map<string, carData>*>& carInfos) {
+    // Percorre todas as rodovias
+    for (auto& [highway, cars] : carInfos) {
+        // Percorre todas as pistas
+        for (int lane = 0; lane < 4; lane++) {
+            // Cria um vetor com os carros da pista atual
+            vector<carData*> carsInLane;
+            for (auto& [name, data] : *cars) {
+                if (data.lane == lane) {
+                    carsInLane.push_back(&data);
+                }
+            }
+
+            // Verifica as colisões entre os carros da pista atual
+            int numCars = carsInLane.size();
+            for (int i = 0; i < numCars; i++) {
+                for (int j = i + 1; j < numCars; j++) {
+                    auto car1 = carsInLane[i];
+                    auto car2 = carsInLane[j];
+
+                    if (car1->speed == car2->speed && car1->acceleration == car2->acceleration) {
+                        int timeToCollision = (car2->actualPosition - car1->actualPosition) / car1->speed;
+                        if (timeToCollision == 0) {
+                            cout << "Os carros " << car1->name << " e " << car2->name << " vão colidir na rodovia " << highway << " na pista " << lane << endl;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+void countCarsByHighway(map<int, map<string, carData>*> carInfos) {
+    for (const auto& p : carInfos) {
+        int key = p.first; // obtém a chave do mapa externo
+        map<string, carData>* carMap = p.second; // obtém o mapa interno
+
+        int carCount = 0;
+        for (const auto& q : *carMap) {
+            carData car = q.second; // obtém o valor do mapa interno
+            carCount++;
+        }
+
+        cout << "Rodovia " << key << " tem " << carCount << " carros" << endl;
+    }
+}
+
---
+++ Project_A1/auxiliar.cpp
@@ -0,0 +1,108 @@
+#include <vector>
+#include <string>
+#include <chrono>
+#include <random>
+#include <thread>
+#include <map>
+#include <mutex>
+#include "Estruturas.h"
+
+using namespace std;
+
+
+void calculateSpeedAndAcceleration(map<int, map<string, carData>*>& carInfos) {
+    for (auto& p : carInfos) {
+        map<string, carData>* carMap = p.second;
+        for (auto& q : *carMap) {
+            carData& car = q.second;
+            int currentPosition = car.actualPosition;
+            int lastPosition = car.lastPosition;
+            int penultimatePosition = car.penultimatePosition;
+            int timeInterval = 1.0; // por enquanto estamos difinindo o intervalo de tempo como 1 segundo (deve ser ajustado para o tempo entre dois estados)
+
+            // Calcula a velocidade
+            double velocity = (currentPosition - lastPosition) / timeInterval;
+            car.speed = static_cast<int>(velocity); // converte a velocidade de double para int e atribui ao campo speed da struct carData
+
+            // Calcula a aceleração
+            double lastVelocity = (lastPosition - penultimatePosition) / timeInterval;
+            double acceleration = (velocity - lastVelocity) / timeInterval;
+            car.acceleration = static_cast<int>(acceleration); // converte a aceleração de double para int e atribui ao campo acceleration da struct carData
+        }
+    }
+}
+
+
+int main() {
+
+    Api& API = Api::getInstance(3,"car.txt");
+
+    int maxBlocks = 5;
+    int iters = 5;
+
+
+    vector<string> _ = getFiles();
+    int init = 0;
+    int n_files = 10;
+    vector<string> files;
+    for(int i = 0; i < 5 * (n_files-1) + 1; i += 5){
+        files.push_back(_[i]);
+    }
+    // vector<string> files = {_.begin() + init, _.begin() + init + n_files};
+    for(string e : files){cout << e << endl;}
+
+
+    auto start = chrono::steady_clock::now();
+    map<int, map<string, carData>*> carInfos;
+    map<int, highwayData*> highwayInfos;
+
+    for (auto file : files) {
+        readFiles(file, 1, &carInfos, &highwayInfos);
+    }
+    auto end = chrono::steady_clock::now();
+    chrono::duration<double> totalTime = end - start;
+    cout << "Total time: " << totalTime.count() << "\n\n";
+
+
+    calculateSpeedAndAcceleration(carInfos);
+
+    for (const auto& p : carInfos) {
+        int key = p.first; // obtém a chave do mapa externo
+        map<string, carData>* carMap = p.second; // obtém o mapa interno
+
+        for (const auto& q : *carMap) {
+            string carName = q.first; // obtém a chave do mapa interno
+            carData car = q.second; // obtém o valor do mapa interno
+
+            // imprime o valor de carData
+            cout << "Rodovia: " << key << ", Nome do carro: " << carName << endl;
+            cout << "Pista: " << car.lane << ", Posição atual: " << car.actualPosition << endl;
+            cout << "Última posição: " << car.lastPosition << ", Penúltima posição: " << car.penultimatePosition << endl;
+            cout << "Está na rodovia? " << (car.isInHighway ? "Sim" : "Não") << endl;
+            cout << "Nome: " << car.name << ", Modelo: " << car.model << ", Ano: " << car.year << endl;
+            cout << "Velocidade: " << car.speed << ", Aceleração: " << car.acceleration << ", Pode colidir? " << (car.canCrash ? "Sim" : "Não") << endl;
+            cout << "-----------------------" << endl;
+        }
+    }
+    cout << "\n\n";
+
+    countCarsByHighway(carInfos);
+
+    cout << "\n\n";
+
+    // checkCollisions(carInfos);
+
+    for (auto& p : highwayInfos){
+        int hw_number = p.first;
+        highwayData * HW = p.second;
+
+        cout << "Rodovia: " << hw_number << endl;
+        cout << "Vel. Max.: " << HW->maxSpeed << " | Carro V.M.: " << HW->carMaxSpeed << " Time: " << HW->infoTime << "\n\n";
+    }
+
+   
+
+    return 0;
+}
+ 
+ 
